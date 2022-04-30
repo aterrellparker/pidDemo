6d1fde988aa667e03d7a51b9e65860ab9aa5b625
ScpCode <1.scpschool.1@gmail.com>
2022-04-30 17:07:46 -0400

    Created Circular PID Controller
    Made Classes/For Pid and ObjectS
    
    Created Angular PID which matches the angle of the mouse relative to the screen center.

--- pid.js
+++ pid.js
@@ -15,19 +15,19 @@
  You should have received a copy of the GNU General Public License
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-(function() {
+(function () {
 	"use strict";
 
 	// default parameter values
-	var proportional_gain = .25;
-	var integral_gain = 0.01;
-	var derivative_gain = 0.3;
-	var control_loop_frequency = 15;
+	var proportionalGain = .15;
+	var integralGain = 0;
+	var derivativeGain = 0.3;
+	var controlFrequency = 15;
 
 	var ctx;
 	var cnvs;
 
-	var cnvs_size = function(dim){
+	var cnvs_size = function (dim) {
 		return {
 			'x': cnvs.width,
 			'y': cnvs.height,
@@ -35,37 +35,38 @@
 	}
 
 	// helper for calling lineTo, moveTo, etc in flipped dimensions
-	var xy_call = function(f,dim,x,y){
-		if(dim === 'x'){
+	var xy_call = function (f, dim, x, y) {
+		if (dim === 'x') {
 			f(x, y);
 		} else {
 			f(y, x);
 		}
 	};
 
-	var setpoint, mouse, ball_position, ball_velocity, error, last_error, integral, num_past_errors, past_errors;
+	var setPoint, mouse, ballPos, ballVelo, ballAccel, error, lastError, integral, pastErrorCount, pastErrors;
 
-	var reset = function(){
-		var middle = { 'x': cnvs_size()['x']/2, 'y': cnvs_size()['y']/2 };
-		setpoint = copy_xy(middle);
-		mouse = { 'x': cnvs_size()['x']/2 + 1, 'y': cnvs_size()['y']/2 };
-		ball_position = copy_xy(middle);
-		ball_velocity = { 'x': -0.01, 'y': 0.01 };
+	var reset = function () {
+		var middle = { 'x': cnvs_size()['x'] / 2, 'y': cnvs_size()['y'] / 2 };
+		setPoint = copy_xy(middle);
+		mouse = { 'x': cnvs_size()['x'] / 2 + 1, 'y': cnvs_size()['y'] / 2 };
+		ballPos = copy_xy(middle);
+		ballVelo = { 'x': -0.01, 'y': 0.01 };
+		ballAccel = { 'x': 0, 'y': 0};
 		error = { 'x': 0, 'y': 0 };
-		last_error = { 'x': 0, 'y': 0 };
+		lastError = { 'x': 0, 'y': 0 };
 		integral = { 'x': 0, 'y': 0 };
-		if(cnvs.width > cnvs.height){
-			num_past_errors = cnvs.width;
+		if (cnvs.width > cnvs.height) {
+			pastErrorCount = cnvs.width;
 		} else {
-			num_past_errors = cnvs.height;
+			pastErrorCount = cnvs.height;
 		}
-		past_errors = [];
-		for(var i = 0; i <= num_past_errors; i++){
-			past_errors.push({'x':0,'y':0});
+		pastErrors = [];
+		for (var i = 0; i <= pastErrorCount; i++) {
+			pastErrors.push({ 'x': 0, 'y': 0 });
 		}
 	}
 
-	var update_mouse = function(e) {
+	var update_mouse = function (e) {
 		var x = 0;
 		var y = 0;
 		if (e.offsetX) {
@@ -79,9 +80,20 @@
 			'x': x,
 			'y': y
 		};
+		mouseAngle();
 	};
+	var iHat = { 'x': 1, 'y': 0 };
+	var jHat = { 'x': 0, 'y': 1 };
+	var mag;
+	var theta;
+	var mouseAngle = function () {
+		mag = Math.sqrt(setPoint['x'] * setPoint['x'] + setPoint['y'] * setPoint['y']);
+		document.getElementById('magMouse').innerHTML = mag;
+		theta = Math.acos(setPoint['x'] / mag) * 180/3.14;
+		document.getElementById('thetaMouse').innerHTML = theta;
 
-	var update_touch = function(e) {
+    };
+	var update_touch = function (e) {
 		e.preventDefault();
 		var x = e.targetTouches[0].pageX - cnvs.offsetLeft;
 		var y = e.targetTouches[0].pageY - cnvs.offsetTop;
@@ -91,113 +103,117 @@
 		};
 	}
 
-	var copy_xy = function(v) {
+	var copy_xy = function (v) {
 		return {
 			'x': v.x,
 			'y': v.y
 		};
 	};
 
-	var control_loop = function() {
-		setpoint = copy_xy(mouse);
-		['x', 'y'].forEach(function(dim){
-			error[dim] = setpoint[dim] - ball_position[dim];
-			integral[dim] = integral[dim] + error[dim]*control_loop_frequency
-			var derivative = (error[dim] - last_error[dim])/control_loop_frequency
-			var output = proportional_gain * error[dim]
-			output += integral_gain * integral[dim]
-			output += derivative_gain * derivative
-			last_error[dim] = error[dim];
-			ball_velocity[dim] = output;
+	var control_loop = function () {
+		setPoint = copy_xy(mouse);
+		['x', 'y'].forEach(function (dim) {
+			document.getElementById('log' + dim).innerHTML = setPoint[dim];
+			error[dim] = setPoint[dim] - ballPos[dim];		
+			integral[dim] = integral[dim] + error[dim] * controlFrequency
+			var derivative = (error[dim] - lastError[dim]) / controlFrequency
+			var output = proportionalGain * error[dim]
+			output += integralGain * integral[dim]
+			output += derivativeGain * derivative
+			lastError[dim] = error[dim];
+			ballVelo[dim] = output;
 		});
 
 		// keep these for charting
-		past_errors.unshift(copy_xy(error));
-		if (past_errors.length > num_past_errors) {
-			past_errors.pop();
+		pastErrors.unshift(copy_xy(error));
+		if (pastErrors.length > pastErrorCount) {
+			pastErrors.pop();
 		}
 
-		setTimeout(control_loop, 1000 / control_loop_frequency);
+		setTimeout(control_loop, 1000 / controlFrequency);
 	};
 
-	var world_tick = function() {
-		['x','y'].forEach(function(dim){
-			ball_position[dim] += ball_velocity[dim];
+	var world_tick = function () {
+		['x', 'y'].forEach(function (dim) {
+			ballPos[dim] += ballVelo[dim];
+			ballVelo[dim] += ballAccel[dim];
 		});
 	};
 
-	var draw_chart = function(dim, color){
+	var draw_chart = function (dim, color) {
 		ctx.strokeStyle = color;
 		ctx.beginPath();
-		xy_call(function(a,b){ctx.moveTo(a,b)}, dim, cnvs_size()[dim] / 2, 0);
-		past_errors.forEach(function(e, i){
-			xy_call(function(a,b){ctx.lineTo(a,b)}, dim, cnvs_size()[dim] / 2 + e[dim], i);
+		xy_call(function (a, b) { ctx.moveTo(a, b) }, dim, cnvs_size()[dim] / 2, 0);
+		pastErrors.forEach(function (e, i) {
+			xy_call(function (a, b) { ctx.lineTo(a, b) }, dim, cnvs_size()[dim] / 2 + e[dim], i);
 		});
 		ctx.stroke();
 	}
 
-	var draw = function() {
+	var draw = function () {
 		ctx.clearRect(0, 0, cnvs.width, cnvs.height);
-
 		ctx.font = "10px sans-serif";
 		ctx.fillStyle = 'green';
 		ctx.fillText("Time ↓", 5, 20);
 		ctx.fillStyle = 'orange';
 		ctx.fillText("Time →", 5, 10);
-		draw_chart('x','green');
-		draw_chart('y','orange');
+		draw_chart('x', 'green');
+		draw_chart('y', 'orange');
+
+		
 
 		ctx.beginPath();
-		ctx.arc(ball_position.x, ball_position.y, 20, 0, Math.PI * 2, true);
+		ctx.arc(ballPos.x, ballPos.y, 20, 0, Math.PI * 2, true);
 		ctx.closePath();
 		ctx.strokeStyle = 'black';
 		ctx.fillStyle = 'red';
 		ctx.fill();
 		ctx.stroke();
+
+		
 	};
 
-	var main_loop = function(){
+	var main_loop = function () {
 		world_tick();
 		draw();
-		update_var();
 		window.requestAnimationFrame(main_loop);
 	}
 
-	var update_vars = function() {
+	var update_vars = function () {
 		if (window.controls.checkValidity()) {
-			proportional_gain = window.proportional_box.value;
-			integral_gain = window.integral_box.value;
-			derivative_gain = window.derivative_box.value;
-			control_loop_frequency = window.frequency_box.value;
-			if (control_loop_frequency > 30){
-				control_loop_frequency = 30;
+			proportionalGain = window.proportional_box.value;
+			integralGain = window.integral_box.value;
+			derivativeGain = window.derivative_box.value;
+			controlFrequency = window.frequency_box.value;
+			if (controlFrequency > 30) {
+				controlFrequency = 30;
 				window.frequency_box.value = 30;
 			}
-			//reset();
+			reset();
 		}
 	};
 
-	var submit = function(e) {
+	var submit = function (e) {
 		e.preventDefault()
 		update_vars()
 	}
 
-	var winch = function() {
+	var winch = function () {
 		cnvs.width = window.innerWidth;
 		cnvs.height = window.innerHeight - window.controls.offsetHeight;
 		update_vars();
 	};
 
-	window.onload = function() {
+	window.onload = function () {
 		window.proportional_box = document.getElementById('proportional_box');
 		window.integral_box = document.getElementById('integral_box');
 		window.derivative_box = document.getElementById('derivative_box');
 		window.frequency_box = document.getElementById('frequency_box');
 		window.controls = document.getElementById('controls');
-		window.proportional_box.value = proportional_gain;
-		window.integral_box.value = integral_gain;
-		window.derivative_box.value = derivative_gain;
-		window.frequency_box.value = control_loop_frequency;
+		window.proportional_box.value = proportionalGain;
+		window.integral_box.value = integralGain;
+		window.derivative_box.value = derivativeGain;
+		window.frequency_box.value = controlFrequency;
 		cnvs = document.getElementById('cnvs');
 		ctx = cnvs.getContext('2d');
 		reset();
---
+++ pidAngular.js
@@ -0,0 +1,188 @@
+//Object Class for the object we are controlling the circular motion of. Gives the object angular properties.
+class weight {
+    constructor(theta = 0, omega = 0, alpha = 0) {
+        //The objects rotation in radians.
+        this.theta = theta;
+        //The object angular velocity in radians/s.
+        this.omega = omega;
+        //The objects angular acceleration in radians/s^2
+        this.alpha = alpha;
+    }
+}
+//A class for the PID controller. Not fully implemented yet only proportional control so far.
+class pid {
+    //Initiliazes constants for PID  and sets a frequency for the controller
+    constructor(proportionalGain, integralGain, derivativeGain, controlFrequency) {
+
+        this.kP = proportionalGain;
+        this.kI = integralGain;
+        this.kD = derivativeGain;
+        this.dt = 1000 / controlFrequency;
+        //Sets initial vairables to zero need to find a better way to avoid exceptions
+        this.output = 0;
+        this.current = 0;
+        this.target = 0;
+        this.error = 0;
+        
+    }
+    //The controll loop which runs repeatedly according to the refresh rate;
+    controlLoop = function () {
+      
+        this.lastError = this.error;
+
+
+        //First and foremost calculates error of the PID controller.
+        this.error = this.target - this.current;
+        //  console.log(this.error)
+
+        if (this.discon = true) {
+            if (this.error > Math.PI) {
+                this.error = -1 * Math.abs(2 * Math.PI - Math.abs(this.error));
+            }
+        }
+        this.output = this.proportional()
+
+        // this.integrals += this.error;
+
+        this.output += this.derivative()
+       
+    }
+
+    proportional = function () {
+        return this.kP * this.error;
+    }
+    
+    integral = function () {
+        return this.kI * this.integrals * this.dt;
+    }
+
+    derivative = function () {
+        
+        this.derivativeVal = this.kD * (this.error - this.lastError) / this.dt
+        console.log(this.derivativeVal)
+        return this.derivativeVal
+    }
+    //Sets the target of the pid controller
+    set setTarget(val) {
+        this.target = val;
+
+    }
+
+    //Sets the current of the pid controller
+    set setCurrent(val) {
+        this.current = val;
+      
+    }
+ 
+
+}
+(function () {
+    var coordinateSystem, trackRadius;
+    let object = new weight(0, 0, 0);
+    let thetaPid = new pid(.03, 0, 0, 10);
+    let omegaPid = new pid(.35, 0, 0, 10);
+    window.onload = function () {
+        cnvs = document.getElementById('cnvs');
+
+        ctx = cnvs.getContext('2d');
+        cnvs.addEventListener('mousemove', updateMouse);
+
+        input = document.querySelector('input');
+        input.addEventListener('change', updateValue);
+
+        window.addEventListener('resize', winch);
+        winch();
+        coordinateSystem = { 'x': cnvs.width / 2, 'y': cnvs.height / 2 };
+        trackRadius = cnvs.height / 2.5;
+        
+        worldTick();
+        thetaPid.discon = true;
+        setInterval(function () { thetaPid.controlLoop() }, thetaPid.dt);
+        setInterval(function () { omegaPid.controlLoop() }, thetaPid.dt);
+    };
+
+    var mTheta;
+
+    var updateMouse = function (e) {
+        var x = 0;
+        var y = 0;
+        if (e.offsetX) {
+            x = e.offsetX;
+            y = e.offsetY;
+        } else if (e.layerX) {
+            x = e.layerX;
+            y = e.layerY;
+        }
+        mouse = {
+            'x': x - coordinateSystem['x'],
+            'y': y - coordinateSystem['y']
+        };
+        mDist = Math.sqrt(mouse['x'] * mouse['x'] + mouse['y'] * mouse['y']);
+        document.getElementById('magMouse').innerHTML = mDist;
+
+        mTheta = Math.atan2(mouse['y'], mouse['x']);
+        thetaPid.setTarget = mTheta;
+        document.getElementById('mouseTheta').innerHTML = mTheta;
+    };
+
+    var winch = function () {
+        cnvs.width = window.innerWidth;
+        cnvs.height = window.innerHeight - window.controls.offsetHeight;
+    };
+  
+
+    function updateValue(e) {
+        targetTheta = e.target.value * Math.PI / 180
+        thetaPid.setTarget = targetTheta;
+    }
+    var worldTick = function () {
+
+        object.theta += object.omega
+
+        thetaPid.current = object.theta
+
+        omegaPid.setTarget = thetaPid.output
+
+        object.omega += object.alpha
+
+        omegaPid.current = object.omega
+
+        object.alpha = omegaPid.output
+
+
+        document.getElementById('objectTheta').innerHTML = object.theta;
+
+
+        document.getElementById('objectOmega').innerHTML = object.omega;
+        draw();
+        window.requestAnimationFrame(worldTick);
+    };
+
+
+    var draw = function () {
+        ctx.clearRect(0, 0, cnvs.width, cnvs.height);
+
+        ctx.strokeStyle = 'black';
+        ctx.lineWidth = 1;
+        ctx.beginPath();
+        ctx.arc(coordinateSystem['x'], coordinateSystem['y'], 1, 0, Math.PI * 2, true);
+        ctx.closePath();
+        ctx.stroke();
+
+        ctx.strokeStyle = 'black';
+        ctx.lineWidth = 5;
+        ctx.beginPath();
+        ctx.arc(coordinateSystem['x'], coordinateSystem['y'], trackRadius, 0, Math.PI * 2, true);
+        ctx.closePath();
+        ctx.stroke();
+
+        ctx.strokeStyle = 'red';
+        ctx.lineWidth = 5;
+        ctx.beginPath();
+        ctx.arc(coordinateSystem['x'] + trackRadius * 1.25 * Math.cos(object.theta), coordinateSystem['y'] + trackRadius * 1.25 * Math.sin(object.theta), 50, 0, Math.PI * 2, true);
+        ctx.closePath();
+        ctx.stroke();
+
+    };
+}).call(this);
+
